0c2a6fa7efb4b7f65245547a75ff291f2d15cc05
Gmes23 <gmesda23@gmail.com>
2017-02-18 01:11:21 -0500

    site can now fetch posts

---
+++ src/components/posts_index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return (
+            <div> list of blog post </div>
+        );
+    }
+
+}
+
+export default connect(null, { fetchPosts }) (PostsIndex);
